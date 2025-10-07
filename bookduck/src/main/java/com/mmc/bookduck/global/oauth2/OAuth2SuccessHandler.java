965e1162475fc6e2c6118aad7e702380d9aef107
Seohyun Lee <applemint2003@gmail.com>
2025-10-07 16:53:34 +0900

    chore: 프론트엔드 배포 주소 변경

--- bookduck/src/main/java/com/mmc/bookduck/global/oauth2/OAuth2SuccessHandler.java
+++ bookduck/src/main/java/com/mmc/bookduck/global/oauth2/OAuth2SuccessHandler.java
@@ -21,10 +21,11 @@ public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
     private final JwtUtil jwtUtil;
     private final CookieUtil cookieUtil;
     private static final String OAUTH_PATH = "/api/oauth";
-    private static final String DEPLOYED_REDIRECT_URL = "https://bookduck-final.vercel.app";
+    private static final String DEPLOYED_REDIRECT_URL = "https://bookduck.co.kr";
     private static final List<String> ALLOWED_REDIRECT_URLS = List.of(
             "http://localhost:3000",
-            "http://localhost:8080"
+            "http://localhost:8080",
+            "https://bookduck-final.vercel.app"
     );
 
     @Override
