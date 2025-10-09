77ef552de0fd4df531b5f4d3956b2ae33b50f62f
jud1thdev <8w8u8@naver.com>
2025-10-10 08:57:16 +0900

    fix: BookCoverImageUnitDto에 author 필드 추가

--- bookduck/src/main/java/com/mmc/bookduck/domain/book/dto/common/BookCoverImageUnitDto.java
+++ bookduck/src/main/java/com/mmc/bookduck/domain/book/dto/common/BookCoverImageUnitDto.java
@@ -5,13 +5,15 @@ import com.mmc.bookduck.domain.book.entity.BookInfo;
 public record BookCoverImageUnitDto(
         Long bookInfoId,
         String imgPath,
-        String title
+        String title,
+        String author
 ){
     public static BookCoverImageUnitDto from(BookInfo bookInfo) {
         return new BookCoverImageUnitDto(
                 bookInfo.getBookInfoId(),
                 bookInfo.getImgPath(),
-                bookInfo.getTitle()
+                bookInfo.getTitle(),
+                bookInfo.getAuthor()
         );
     }
 }
